99da6ed30c034c2ebf318b62ee1b17a7e3322b41
Lucas-Anseloni <lucasanseloni@ciandt.com>
2022-06-02 22:20:14 -0300

    Teste unitario

--- 0x00/email/Pessoa.java
+++ 0x00/email/Pessoa.java
@@ -1,4 +1,11 @@
-package email;
+
 
 public class Pessoa {
+
+    public static boolean emailValid(String email){
+        if (email.contains("@") && email.length() < 50){
+            return true;
+        }
+        return false;
+    }
 }
